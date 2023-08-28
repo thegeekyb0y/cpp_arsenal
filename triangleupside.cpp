11c7472b3db6ec19326089c09b531d56b22977a0
Aditya Tiwari <thegeekyboy.in@gmail.com>
2023-08-29 02:10:12 +0530

    Coded upside triangle pattern

---
+++ triangleupside.cpp
@@ -0,0 +1,26 @@
+#Output 
+
+x
+xx
+xxx
+xxxx
+xxxxx
+
+------------------------------------------------------------------------
+# Code goes here:
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int row;
+  cin >> row;
+  
+  for (int i = 1 ; i <= row ; i++) {
+    for (int j = 1 ; j <= i ; j++) {
+      cout << "x";
+    }
+    cout << endl;
+  }
+}
