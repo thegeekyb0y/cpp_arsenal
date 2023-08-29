43f616dfe87cd44b0dbb8b3998667a2a7778805e
Aditya Tiwari <thegeekyboy.in@gmail.com>
2023-08-30 00:41:08 +0530

    Create trianglecenter.cpp

---
+++ trianglecenter.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int rows;
+  cin >> rows;
+  
+  for (int i = 1 ; i <= rows ; i++) {
+    for (int j = 1 ; j <= (rows - i) ; j++) {
+      cout << " ";
+    }
+    for (int k = 1; k <= ( 2 * i - 1) ; k++) {
+      cout << "x";
+    }
+    cout << endl;
+  }
+}
