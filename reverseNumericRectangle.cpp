b023fb830650179ede10b113870859cb985b1663
Aditya Tiwari <thegeekyboy.in@gmail.com>
2023-09-06 00:07:52 +0530

    Create reverseNumericRectangle.cpp

---
+++ reverseNumericRectangle.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  int count = 1;
+  cout << "Enter a number ";
+  cin >> n;
+  for (int i=1; i<=n ; i++) {
+    for (int j = 1 ; j<=n ; j++) {  
+      cout << n-j+1; 
+      
+    }
+  cout << endl;  
+  }
+}
