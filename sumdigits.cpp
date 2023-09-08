ed0b88af7cc13e219e67b1e0c33d57edfd8f2bba
Aditya Tiwari <thegeekyboy.in@gmail.com>
2023-09-09 00:54:25 +0530

    Create sumdigits.cpp

---
+++ sumdigits.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  int sum = 0;
+  int lastdigit;
+  while (n>0) {
+    lastdigit=n%10;
+    sum = sum+lastdigit ;
+    n = n/10;
+  }
+  cout << "Sum of " << n << "is " << sum;
+}
